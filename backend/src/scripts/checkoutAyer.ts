f5cb974bc6295eb11e7073f0bb9c76ad6dd53d33
smartinezdisciple <smartinezdisciple@github.com>
2026-06-22 14:06:28 -0500

    chore(scripts): agregar soporte de SSL en conexion a base de datos de Neon

--- backend/src/scripts/checkoutAyer.ts
+++ backend/src/scripts/checkoutAyer.ts
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   user:     process.env.PGUSER,
   password: process.env.PGPASSWORD,
+  ssl:      { rejectUnauthorized: false }
 });
 
 async function run() {
